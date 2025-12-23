94fbff9722c020ab514d77fbb23ced3d976316f7
Manoj vuddanti <manojvuddanti135@gmail.com>
2025-12-23 04:24:59 +0100

    changes made for file upload api endpoint

--- src/main/java/com/manoj/Loganalyzer/controller/LogController.java
+++ src/main/java/com/manoj/Loganalyzer/controller/LogController.java
@@ -3,7 +3,12 @@ package com.manoj.Loganalyzer.controller;
 import com.manoj.Loganalyzer.model.LogSummary;
 import com.manoj.Loganalyzer.service.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/validatelogsapi/logs")
@@ -15,5 +20,15 @@ public class LogController {
     @PostMapping("/analyze")
     public LogSummary analyze(@RequestBody String logs) {
         return logService.analyzeLogs(logs);
+    }
+    @PostMapping("/upload")
+    public ResponseEntity<LogSummary> uploadLogFile(@RequestParam("file") MultipartFile file) {
+        try {
+            String content = new String(file.getBytes());
+            LogSummary summary = logService.analyzeLogs(content);
+            return ResponseEntity.ok(summary);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
     }
 }
